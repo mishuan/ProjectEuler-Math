f973a91e1167882a725e4f8a22359135212f9b56
Michael Yuan <yuanshihao1994@gmail.com>
2014-05-27 23:32:51 -0400

    Problem 43

---
+++ Euler/src/eulerproblems/P043.java
@@ -0,0 +1,31 @@
+package eulerproblems;
+
+import utility.CommonMethods;
+
+
+public class P043 {
+
+   public static void main(String[] args) {
+      int[] digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+      int[] primes = {2, 3, 5, 7, 11, 13, 17};
+      long sum = 0;
+      do {
+         boolean complete = true;
+         for (int i = 7; i > 0; i--) {
+            int num = digits[2 + i] + 10 * digits[1 + i] + 100 * digits[i];
+            if (num % primes[i - 1] != 0) {
+               complete = false;
+               break;
+            }
+         }
+         if (complete) {
+            long multiplier = 1;
+            for (long i = 9; i >= 0; i--) {
+               sum += digits[(int)i] * multiplier;
+               multiplier *= 10;
+            }
+         }
+      } while (CommonMethods.nextPermutation(digits));
+      System.out.println(sum);
+   }
+}
--- Euler/src/utility/CommonMethods.java
+++ Euler/src/utility/CommonMethods.java
@@ -60,4 +60,26 @@ final public class CommonMethods {
       return a + b;
    }
 
+   public static boolean nextPermutation(int[] p) {
+      int a = p.length - 2;
+      while (a >= 0 && p[a] >= p[a + 1]) {
+         a--;
+      }
+      if (a == -1) {
+         return false;
+      }
+      int b = p.length - 1;
+      while (p[b] <= p[a]) {
+         b--;
+      }
+      int t = p[a];
+      p[a] = p[b];
+      p[b] = t;
+      for (int i = a + 1, j = p.length - 1; i < j; i++, j--) {
+         t = p[i];
+         p[i] = p[j];
+         p[j] = t;
+      }
+      return true;
+   }
 }
