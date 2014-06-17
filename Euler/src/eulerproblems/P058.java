49f8b7ffa9ec3c9cade9a1555c1665086fa41abe
Michael Yuan <yuanshihao1994@gmail.com>
2014-06-17 00:48:57 -0400

    Problem 58 -original solution too slow -used someone else's implementation of Rabin-Miller test -credits go to Kristian of mathblog.dk

--- Euler/src/eulerproblems/P058.java
+++ Euler/src/eulerproblems/P058.java
@@ -5,9 +5,18 @@ import utility.CommonMethods;
 public class P058 {
 
    public static void main(String[] args) {
-      boolean[] isPrime = CommonMethods.primeGenerator(5000000);
-      int layer = 2;
+      int i = 2;
       int primes = 3;
-      
+      int curCorner = 9;
+      while ((double) primes / (double) (2 * i + 1) > 0.10d) {
+         i += 2;
+         for (int j = 0; j < 3; j++) {
+            curCorner += i;
+            if (CommonMethods.isPseudoPrime(curCorner))
+               primes++;
+         }
+         curCorner += i;
+      }
+      System.out.println(i + 1);
    }
 }
--- Euler/src/utility/CommonMethods.java
+++ Euler/src/utility/CommonMethods.java
@@ -187,4 +187,65 @@ final public class CommonMethods {
       }
       return reversed;
    }
+
+   public static boolean isPseudoPrime(int n) {
+      if (n <= 1)
+         return false;
+      if (n == 2)
+         return true;
+      if (n % 2 == 0)
+         return false;
+      if (n < 9)
+         return true;
+      if (n % 3 == 0)
+         return false;
+      if (n % 5 == 0)
+         return false;
+
+      int[] ar = new int[] {2, 3};
+      for (int i = 0; i < ar.length; i++) {
+         if (Witness(ar[i], n))
+            return false;
+      }
+      return true;
+   }
+
+
+   private static boolean Witness(int a, int n) {
+      int t = 0;
+      int u = n - 1;
+      while ((u & 1) == 0) {
+         t++;
+         u >>= 1;
+      }
+
+      long xi1 = ModularExp(a, u, n);
+      long xi2;
+
+      for (int i = 0; i < t; i++) {
+         xi2 = xi1 * xi1 % n;
+         if ((xi2 == 1) && (xi1 != 1) && (xi1 != (n - 1)))
+            return true;
+         xi1 = xi2;
+      }
+      if (xi1 != 1)
+         return true;
+      return false;
+   }
+
+
+   private static long ModularExp(int a, int b, int n) {
+      long d = 1;
+      int k = 0;
+      while ((b >> k) > 0)
+         k++;
+
+      for (int i = k - 1; i >= 0; i--) {
+         d = d * d % n;
+         if (((b >> i) & 1) > 0)
+            d = d * a % n;
+      }
+
+      return d;
+   }
 }
