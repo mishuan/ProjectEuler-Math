1981bba54238e576462a07b377180df7bacd2dbb
Michael Yuan <yuanshihao1994@gmail.com>
2014-06-16 23:31:34 -0400

    Problem 58 init

---
+++ Euler/src/eulerproblems/P058.java
@@ -0,0 +1,13 @@
+package eulerproblems;
+
+import utility.CommonMethods;
+
+public class P058 {
+
+   public static void main(String[] args) {
+      boolean[] isPrime = CommonMethods.primeGenerator(5000000);
+      int layer = 2;
+      int primes = 3;
+      
+   }
+}
