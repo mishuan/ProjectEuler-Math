1d447e229141e516781522624df1e3f41e543ffa
Michael Yuan <yuanshihao1994@gmail.com>
2014-06-19 00:00:46 -0400

    Problem 145 -im unsure whether my solution is correct or not -project euler is down

--- Euler/src/eulerproblems/P145.java
+++ Euler/src/eulerproblems/P145.java
@@ -1,11 +1,34 @@
 package eulerproblems;
 
+import utility.CommonMethods;
+
 public class P145 {
 
    public static void main(String[] args) {
-      int current = 1;
-      
-
+      int counter = 0;
+      boolean[] checked = new boolean[1000000000];
+      for (int i = 1; i < 1000000000; i += 2) {
+         if (checked[i])
+            continue;
+         int reverse = CommonMethods.reverseNumber(i);
+         checked[i] = true;
+         checked[reverse] = true;
+         int sum = reverse + i;
+         boolean isReversible = true;
+         while (sum > 0) {
+            if ((sum % 10) % 2 == 0) {
+               isReversible = false;
+               break;
+            }
+            sum /= 10;
+         }
+         if (isReversible) {
+            if (CommonMethods.isPalindrome(String.valueOf(i)))
+               counter += 2;
+            else
+               counter++;
+         }
+      }
+      System.out.println(counter);
    }
-
 }
