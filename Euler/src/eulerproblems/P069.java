ba3250b33186ade8a107822cf6475a64f39feae7
Michael Yuan <yuanshihao1994@gmail.com>
2014-06-23 23:08:41 -0400

    Problem 69 Start

---
+++ Euler/src/eulerproblems/P069.java
@@ -0,0 +1,11 @@
+package eulerproblems;
+
+import utility.CommonMethods;
+
+public class P069 {
+
+   public static void main(String[] args) {
+      int[] primes = CommonMethods.getPrimeArray(CommonMethods.primeGenerator(1000000));
+   }
+
+}
