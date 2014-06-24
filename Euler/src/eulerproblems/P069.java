b493b4a6d6e4de50fd5a3cedfc3f6ea278057e35
Michael Yuan <yuanshihao1994@gmail.com>
2014-06-24 18:19:13 -0400

    Problem 69

--- Euler/src/eulerproblems/P069.java
+++ Euler/src/eulerproblems/P069.java
@@ -6,6 +6,12 @@ public class P069 {
 
    public static void main(String[] args) {
       int[] primes = CommonMethods.getPrimeArray(CommonMethods.primeGenerator(1000000));
+      int result = 1;
+      int index = 0;
+      while (result * primes[index] < 1000000) {
+         result *= primes[index];
+         index++;
+      }
+      System.out.println(result);
    }
-
 }
