b8d2200160cd5bc5c4bc315068d1d3db86777470
DrCalculus <yuanshihao1994@gmail.com>
2014-07-06 22:28:24 -0400

    Problem 81 progress -finish when i get home

--- Euler/src/eulerproblems/P081.java
+++ Euler/src/eulerproblems/P081.java
@@ -1,6 +1,9 @@
 package eulerproblems;
 
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 import utility.CommonMethods;
 
@@ -15,5 +18,26 @@ public class P081 {
             matrix[i][j] = Integer.valueOf(split[j]);
          }
       }
+      Queue<Integer[]> queue = new PriorityQueue<Integer[]>(6400, arrayComparator);
+      Integer[] pushArray1 = new Integer[3];
+      pushArray1[0] = 0;
+      pushArray1[1] = 0;
+      pushArray1[2] = matrix[0][0];
+      queue.add(pushArray1);
+      while (true) {
+         Integer[] poppedArray = queue.poll();
+         if (poppedArray[0] == 79 && poppedArray[1] == 79) {
+            System.out.println(poppedArray[2]);
+            break;
+         }
+      }
    }
+
+   public static Comparator<Integer[]> arrayComparator = new Comparator<Integer[]>() {
+
+      @Override
+      public int compare(Integer[] i1, Integer[] i2) {
+         return (int) (i1[2] - i2[2]);
+      }
+   };
 }
