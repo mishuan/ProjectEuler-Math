b31716e9f3cfac4dbf963c6cad95dbf0efb79c26
DrCalculus <yuanshihao1994@gmail.com>
2014-07-14 00:14:27 -0400

    Problem 96 -done

--- Euler/src/eulerproblems/P096.java
+++ Euler/src/eulerproblems/P096.java
@@ -2,12 +2,30 @@ package eulerproblems;
 
 import java.io.IOException;
 
+import utility.CommonMethods;
 import utility.SudokuSearch;
 
 public class P096 {
 
    public static void main(String[] args) throws IOException {
       SudokuSearch oracle = new SudokuSearch();
-      System.out.println(oracle.search("003020600900305001001806400008102900700000008006708200002609500800203009005010300"));
+      String[] originalFile = CommonMethods.fileToStringArray("sudoku");
+      String[] grids = new String[50];
+      String s = "";
+      int solution = 0;
+      for (int i = 1, j = 0; i < 500; i++) {
+         if (i % 10 == 0) {
+            grids[j] = s;
+            j++;
+            s = "";
+         } else
+            s = s + originalFile[i];
+      }
+      grids[49] = s;
+      long init = System.currentTimeMillis();
+      for (String sol : grids) {
+         solution += Integer.valueOf(oracle.search(sol).substring(0, 3));
+      }
+      System.out.println("Sum is " + solution + " Found in: " + (System.currentTimeMillis() - init) + "ms");
    }
 }
