dac57f6521efc9e77a84014888b040f2a6ab1e74
DrCalculus <yuanshihao1994@gmail.com>
2014-07-21 16:39:50 -0400

    Problem 104 -very novel way of solving this problem, TIL

--- Euler/src/eulerproblems/P104.java
+++ Euler/src/eulerproblems/P104.java
@@ -1,10 +1,31 @@
 package eulerproblems;
 
+import utility.CommonMethods;
+
 public class P104 {
 
    public static void main(String[] args) {
-      // TODO Auto-generated method stub
-
+      long f0 = 1;
+      long f1 = 1;
+      long f2;
+      int count = 2;
+      while (true) {
+         f2 = (f0 + f1) % 1000000000;
+         count++;
+         f0 = f1;
+         f1 = f2;
+         if (count < 2750)
+            continue;
+         if (CommonMethods.isPandigital(Long.valueOf(f2))) {
+            // if we want to find the first 9 digits of a number, we find the log of the large
+            // number then split it up into 10^8*10^f where f is some floating point containing
+            // information about the decimal point. for example log(12345) = 4.091491094...
+            // 10^0.91491094... = 1.2345
+            double log = (count * 0.20898764024997873) - 0.3494850021680094;
+            if (CommonMethods.isPandigital((long) Math.pow(10, log - (long) log + 8)))
+               break;
+         }
+      }
+      System.out.println(count);
    }
-
 }
