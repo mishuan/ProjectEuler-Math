1050eb1195d45b5b801d47f4d6afe14842a983b3
DrCalculus <yuanshihao1994@gmail.com>
2014-07-27 02:03:04 -0400

    Problem 31

--- Euler/src/eulerproblems/P031.java
+++ Euler/src/eulerproblems/P031.java
@@ -3,8 +3,14 @@ package eulerproblems;
 public class P031 {
 
    public static void main(String[] args) {
-      // TODO Auto-generated method stub
-
+      int[] dominations = {1, 2, 5, 10, 20, 50, 100, 200};
+      int[] combinations = new int[201];
+      combinations[0] = 1;
+      for (int i = 0; i < dominations.length; i++) {
+         for (int j = dominations[i]; j <= 200; j++) {
+            combinations[j] += combinations[j - dominations[i]];
+         }
+      }
+      System.out.println(combinations[200]);
    }
-
 }
---
+++ Euler/src/eulerproblems/P076.java
@@ -0,0 +1,12 @@
+package eulerproblems;
+
+public class P076 {
+
+   public static void main(String[] args) {
+      int[] combinations = new int[101];
+      combinations[1] = 1;
+      for (int i = 2; i <= 100; i++) {
+      }
+      System.out.println(combinations[100]);
+   }
+}
