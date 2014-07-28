2779a86e945063b5c0d027d80a368470d006d6ef
DrCalculus <yuanshihao1994@gmail.com>
2014-07-28 18:51:37 -0400

    Problem 76 -same as question 31

--- Euler/src/eulerproblems/P076.java
+++ Euler/src/eulerproblems/P076.java
@@ -3,9 +3,12 @@ package eulerproblems;
 public class P076 {
 
    public static void main(String[] args) {
-      int[] combinations = new int[101];
-      combinations[1] = 1;
-      for (int i = 2; i <= 100; i++) {
+      long[] combinations = new long[101];
+      combinations[0] = 1;
+      for (int i = 1; i < 100; i++) {
+         for (int j = i; j < 101; j++) {
+            combinations[j] += combinations[j - i];
+         }
       }
       System.out.println(combinations[100]);
    }
