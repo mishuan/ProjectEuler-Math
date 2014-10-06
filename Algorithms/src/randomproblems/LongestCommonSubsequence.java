c0837adfcb44a74f8a567389264120c442ca546a
DrCalculus <yuanshihao1994@gmail.com>
2014-10-06 13:44:54 -0400

    Longest Common Subsequence

--- Algorithms/src/randomproblems/LongestCommonSubsequence.java
+++ Algorithms/src/randomproblems/LongestCommonSubsequence.java
@@ -7,14 +7,20 @@ public class LongestCommonSubsequence {
       int seq1Length = seq1.length;
       int seq2Length = seq2.length;
       int[][] grid = new int[seq1Length + 1][seq2Length + 1];
-      int largestSequence = 0;
       for (int i = 0; i < seq1Length; i++) {
          for (int j = 0; j < seq2Length; j++) {
-
+            int x = i + 1;
+            int y = j + 1;
+            if (seq1[i] == seq2[j])
+               grid[x][y] = 1 + grid[i][j];
+            else
+               grid[x][y] = Math.max(grid[i][y], grid[x][j]);
          }
       }
-      return largestSequence;
+      return grid[seq1Length][seq2Length];
    }
 
-   public static void main(String[] args) {}
+   public static void main(String[] args) {
+      System.out.println(findLongestSubsequence("ayuanbc","yabuanc"));
+   }
 }
