3568b02edc0ca7cf3864e096ed678a93bf35c45a
DrCalculus <yuanshihao1994@gmail.com>
2014-10-30 18:21:52 -0400

    Dortmund Dilemma - Progress

--- Algorithms/src/hackerrank/DortmundDilemma.java
+++ Algorithms/src/hackerrank/DortmundDilemma.java
@@ -1,21 +1,35 @@
 package hackerrank;
 
 import java.math.BigInteger;
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class DortmundDilemma {
    static int[] combs = new int[27];
    static BigInteger MOD_CONST = BigInteger.valueOf(1000000009);
-   //static HashMap<String, Integer> powers = new HashMap<String, Integer>();
+   static long MOD_CONST_LONG = 1000000009;
+
    public static int possibleNames(int N, int K) {
       long total = 0;
-      BigInteger currPower = BigInteger.ONE;
-      BigInteger uniqueChars = BigInteger.valueOf(K);
-      for (int i = 0; i < N / 2; i++) {
-         currPower.multiply(uniqueChars).mod(MOD_CONST);
+      long currPower = 1;
+      for (int i = 1; i <= N / 2; i++) {
+         currPower = (currPower * K) % MOD_CONST_LONG;
+         int middleDigits = N - 2 * i;
+         long currTotal = 1;
+         // calculate middle digit possibilities
+         if (middleDigits != 0) {
+            if (middleDigits > 1) {
+               for (int j = 1; j < middleDigits; j++)
+                  currTotal = (currTotal * K) % MOD_CONST_LONG;
+               currTotal *= K - 1;
+            } else
+               currTotal = K;
+         }
+         //find total possibilities
+         currTotal = (currTotal * currPower) % MOD_CONST_LONG;
+         total += currTotal;
       }
-      return 0;
+      total = (total % MOD_CONST_LONG) * combs[K];
+      return (int) (total % MOD_CONST_LONG);
    }
 
    public static int combinations(int int0, int int1) {
@@ -34,16 +48,17 @@ public class DortmundDilemma {
    }
 
    public static void main(String[] args) {
-      @SuppressWarnings("resource")
-      Scanner in = new Scanner(System.in);
+//      @SuppressWarnings("resource")
+//      Scanner in = new Scanner(System.in);
       for (int i = 0; i < 27; i++) {
          combs[i] = combinations(26, i);
       }
-      int numInputs = in.nextInt();
-      for (int i = 0; i < numInputs; i++) {
-         int N = in.nextInt();
-         int K = in.nextInt();
-         System.out.println(possibleNames(N, K));
-      }
+//      int numInputs = in.nextInt();
+//      for (int i = 0; i < numInputs; i++) {
+//         int N = in.nextInt();
+//         int K = in.nextInt();
+//         System.out.println(possibleNames(N, K));
+//      }
+      System.out.println(possibleNames(2,2));
    }
 }
